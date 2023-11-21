51f7cad287f3311361307fe513c1e16554ad877d
KAkos27 <akoskosztolanyi@gmail.com>
2023-11-21 16:15:23 +0100

    elso2

---
+++ feladatok.py
@@ -0,0 +1,17 @@
+def elso():
+    szamlalo:int = 0
+    for i in range(1,1001,1):
+        if i % 7 == 0:
+            szamlalo+=1
+    return szamlalo
+
+def masodik():
+    szamlalo:int =0
+    for i in range(2000,20001,1):
+        if i % 12 == 0:
+            szamlalo +=1
+    return szamlalo
+
+def harmadik():
+    
+    
---
+++ main.py
@@ -0,0 +1,7 @@
+import feladatok
+
+elso:int = feladatok.elso()
+print(elso)
+
+masodik:int = feladatok.masodik()
+print(masodik)
