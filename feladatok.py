9f3fe166c25b899c6e45b8862a41918f6ec78b98
KAkos27 <akoskosztolanyi@gmail.com>
2023-11-22 08:10:35 +0100

    primes

--- feladatok.py
+++ feladatok.py
@@ -13,5 +13,31 @@ def masodik():
     return szamlalo
 
 def harmadik():
+    osszeg:int = 0
+    for i in range(3,61,3):
+        osszeg+=i**2
+    return osszeg
+
+def negyedik(szam):
+    print(f"A(z) {szam} osztói: ")
+    for i in range(1,szam + 1,1):
+        if szam % i == 0:
+            if szam != i:
+                print(i, end=", ")
+            else:
+                print(i, end="")
+
+def hatodik(szam):
+    primindex:int = 0
+    prim:str = f"A(z) {szam} egy prímszám"
+    nemprim:str = f"A(z) {szam} nem egy prímszám"
     
+    for i in range(1,szam + 1,1):
+        if szam % i == 0:
+            primindex+=1
     
+    if primindex == 2:
+        return prim
+    else:
+        return nemprim
+       
--- main.py
+++ main.py
@@ -5,3 +5,12 @@ print(elso)
 
 masodik:int = feladatok.masodik()
 print(masodik)
+
+harmadik:int = feladatok.harmadik()
+print(harmadik)
+
+feladatok.negyedik(90)
+print()
+
+hatodik:str = feladatok.hatodik(9)
+print(hatodik)
